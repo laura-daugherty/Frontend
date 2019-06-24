dae549be791a417b4ca6cc70cba84607672bbaec
mochibot <amusaur@gmail.com>
2019-06-24 15:05:32 -0700

    WIP: added reducer and action backbone

--- ill-serve-soup/src/testData.js
+++ ill-serve-soup/src/testData.js
@@ -1,5 +1,6 @@
-const dummyData = [
+export const dummyData = [
   {
+    id: 1,
     username: 'test',
     token: 'testing',
     items: [
@@ -7,6 +8,7 @@ const dummyData = [
         id: 1,
         name: 'beef',
         quantity: 1,
+        unit: 'pound',
         threshold: 2,
         category: 'MEAT' 
       },
@@ -14,9 +16,11 @@ const dummyData = [
         id: 2,
         name: 'flour',
         quantity: 5,
+        unit: 'bag',
         threshold: 2,
         category: 'Dry Goods' 
       },
     ]
   }
 ]
+
