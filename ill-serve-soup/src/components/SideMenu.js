29f64835c528167246bab813d1cf0e770ca40034
mochibot <amusaur@gmail.com>
2019-06-27 08:58:06 -0700

    WIP: fixed bug to set default category to all

--- ill-serve-soup/src/components/SideMenu.js
+++ ill-serve-soup/src/components/SideMenu.js
@@ -3,9 +3,10 @@ import React from 'react';
 const SideMenu = (props) => {
   return(
     <div className='sidemenu-container'>
-      {props.categories.map(item => <div className='sidemenu-btn' onClick={(event) => props.setFilter(event, item)} key={item} style={{background : props.searchCategory === item ? '#C3D4A0' : 'white'}}>{item.split('_').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')}</div>)}
+      {props.categories.map(item => <div className={`sidemenu-btn ${props.searchCategory === item ? 'selected' : ''}`}  onClick={(event) => props.setFilter(event, item)} key={item}>{item.split('_').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')}</div>)}
     </div>
   )
 }
 
 export default SideMenu;
+
